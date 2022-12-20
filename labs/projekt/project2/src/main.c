9a84d0944ff35a5ad32d4dc133a19a178a029505
AnnaLit23 <99733524+AnnaLit23@users.noreply.github.com>
2022-12-20 14:07:44 +0100

    Update main.c

--- labs/projekt/project2/src/main.c
+++ labs/projekt/project2/src/main.c
@@ -4,14 +4,7 @@
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  * 
  *
-/* Define pins for motor 1 ------------------------------------------*/
-#define servo1 PB1 
 
-/* Define pins for motor 2 ---------------------------------------------*/
-#define servo2 PB2
-
-/* Define pins for button ---------------------------------------------*/
-#define BUTTON1 PB0          // Button 1
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
@@ -21,48 +14,54 @@
 #include "timer.h"          // Timer library for AVR-GCC
 #include <stdlib.h>         // C library. Needed for number conversions
 
+/* Define pins  ------------------------------------------*/
+#define servo1 PB1           // Motor 1
+
+#define servo2 PB2           // Motor 2
+
+#define BUTTON1 PB0          // Button 1
+
 
 
 /* Define parameters for servo ---------------------------------------------------------*/
-# define ZERO_ANGLE 124
+# define ANGLE_ZERO 124
 # define ANGLE_180 260
 # define SHORT_DELAY 2000 // Delay in milliseconds
 
 volatile uint32_t servo1_position = ANGLE_180;
 volatile uint32_t servo2_position = ANGLE_180;
-volatile uint8_t MOVE = 0;
+volatile uint8_t RUN = 0;
 
 int main(void)
 {
     /* ----------------------------- Timer for SERVO 1 ---------------------------------*/
-    // Configure 8-bit Timer/Counter0 to control servo motors
-    // Set prescaler to 16 ms and enable overflow interrupt
     TIM0_overflow_16ms();
     TIM0_overflow_interrupt_enable();
 
     /* ------------------------- Set pins with gpio library ----------------------------*/
-    GPIO_mode_output(&DDRB, servo1);        // First servo motor
-    GPIO_mode_output(&DDRB, servo2);        // Second servo motor
-    GPIO_mode_input_pullup(&DDRB,BUTTON1);  // Button with enabled pull-up resistor
+    GPIO_mode_output(&DDRB, servo1);        // 1. servo motor
+    GPIO_mode_output(&DDRB, servo2);        // 2. servo motor
+    GPIO_mode_input_pullup(&DDRB,BUTTON1);  // set Button with enabled pull-up resistor
 
     // Set 10. waveform generation mode (1010)
     TCCR1A |= (1 << WGM11);                  
     TCCR1B |= (1 << WGM13);
 
-    // Set compare output mode
+    // Set compare output
     TCCR1A |= (1 << COM0A1) | (1 << COM0B1); 
 
      // Set TOP value
     ICR1 = 2500;
+    
                          
     // Set duty cycle
     OCR1A = servo1_position;
     OCR1B = servo2_position;
     
-    // Set prescaler to 64
+    // Prescaler to 64
     TCCR1B |= (1 << CS11) | (1 << CS10 ); 
 
-    PCICR |= (1<<PCIE0);                    // Any change of any enable PCINT[7:0] pins will cause an interrupt
+    PCICR |= (1<<PCIE0);                    // Interruotion due to changes in any enable PCINT [7:0] pins
     PCMSK0 |= (1<<PCINT0);                  // Enable PCINT0 change interrupt  
     _delay_ms(SHORT_DELAY); // Wait 2 s
 
@@ -92,14 +91,14 @@ ISR(TIMER0_OVF_vect)
     static uint8_t servo1_direction = 0;        // Default direction setup for first servo motor
     static uint8_t servo2_direction = 0;        // Default direction setup for second servo motor
     /* ------------- First servo motor  ------------------------------*/
-    if (MOVE == 0)                                                      
+    if (RUN == 0)                                                      
     {
         // Direction
         if (servo1_position == ANGLE_180)       // Maximum value
             {
                 servo1_direction = 1;           // Clockwise direction
             }
-        else if (servo1_position == ZERO_ANGLE) // Minimum value
+        else if (servo1_position == ANGLE_ZERO) // Minimum value
             {
                 servo1_direction = 0;           // Counterclockwise direction
             }
@@ -119,7 +118,7 @@ ISR(TIMER0_OVF_vect)
         {
             servo2_direction = 1;               // Clockwise direction
         }
-        else if (servo2_position == ZERO_ANGLE) // Minimum value
+        else if (servo2_position == ANGLE_ZERO) // Minimum value
         {
             servo2_direction = 0;               // Counterclockwise direction
         }
@@ -147,13 +146,13 @@ ISR(PCINT0_vect)
     uint8_t sw1 = GPIO_read(&PINB, BUTTON1);     // Read value from digital pin 8
     if (sw1 == 0)                              // Button is pushed down  
     {
-        if (MOVE == 1) 
+        if (RUN == 1) 
         { 
-            MOVE = 0;                            
+            RUN = 0;                            
         }
-        else if (MOVE == 0) 
+        else if (RUN == 0) 
         { 
-            MOVE = 1;
+            RUN = 1;
         }
     } 
 }
